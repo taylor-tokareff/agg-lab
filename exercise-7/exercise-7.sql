693bf2b04a2ef46e6499308b63ef029b57f68112
taylor-tokareff <taylortokareff@gmail.com>
2021-06-24 11:47:27 -0700

    test 7 passes

--- exercise-7/exercise-7.sql
+++ exercise-7/exercise-7.sql
@@ -1,2 +1,11 @@
 -- customer ranking by total film time watched
 -- similar to exercise 6
+SELECT email,
+  RANK() OVER (
+    ORDER BY SUM(length) DESC
+  )
+FROM customer
+  INNER JOIN rental ON customer.customer_id = rental.customer_id
+  INNER JOIN inventory ON rental.inventory_id = inventory.inventory_id
+  INNER JOIN film ON inventory.film_id = film.film_id
+GROUP BY email
