61d45445d79ff3be293edddf6ad5b032d45dd98c
taylor-tokareff <taylortokareff@gmail.com>
2021-06-24 11:27:26 -0700

    6 test pass

--- exercise-6/exercise-6.sql
+++ exercise-6/exercise-6.sql
@@ -1,3 +1,11 @@
 -- customer ranking by total amount spent
 -- include email and rank
 -- use RANK ORDER BY the SUM of the amount in descending order
+SELECT email,
+  RANK() OVER (
+    ORDER BY SUM(amount) DESC
+  )
+FROM customer
+  INNER JOIN rental ON customer.customer_id = rental.customer_id
+  INNER JOIN payment ON rental.rental_id = payment.rental_id
+GROUP BY email
