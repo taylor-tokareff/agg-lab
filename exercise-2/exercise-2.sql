d6c0716a6c0bdee4a99e0181a5dc0dd97db5d616
taylor-tokareff <taylortokareff@gmail.com>
2021-06-24 11:00:54 -0700

    test 2 passes

--- exercise-2/exercise-2.sql
+++ exercise-2/exercise-2.sql
@@ -1,3 +1,13 @@
 -- the top 10 cities with the most total rental payment amount
 -- include the city name and sum
 -- use SUM to sum up the amounts
+SELECT city,
+  SUM(amount)
+FROM city
+  INNER JOIN address ON address.city_id = city.city_id
+  INNER JOIN customer ON customer.address_id = address.address_id
+  INNER JOIN rental ON rental.customer_id = customer.customer_id
+  INNER JOIN payment ON rental.rental_id = payment.rental_id
+GROUP BY city.city
+ORDER BY SUM DESC
+LIMIT 10
