f5e798a57ad6f3022d19201ed11a8e1926a189e6
taylor-tokareff <taylortokareff@gmail.com>
2021-06-24 11:00:28 -0700

    test 1 passes

--- exercise-1/exercise-1.sql
+++ exercise-1/exercise-1.sql
@@ -3,3 +3,12 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+
+
+SELECT title, COUNT(*)
+FROM rental
+INNER JOIN inventory ON rental.inventory_id = inventory.inventory_id
+INNER JOIN film on film.film_id = inventory.film_id
+GROUP BY film.film_id
+ORDER BY COUNT DESC
+LIMIT 10
